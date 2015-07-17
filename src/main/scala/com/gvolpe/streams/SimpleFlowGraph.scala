d80822e3df3cd4fc1b8f27cd566b114ca45c9450
Gabi Volpe <volpegabriel@gmail.com>
2015-07-17 11:39:12 +0100

    Another example.

--- src/main/scala/com/gvolpe/streams/SimpleFlowGraph.scala
+++ src/main/scala/com/gvolpe/streams/SimpleFlowGraph.scala
@@ -4,10 +4,16 @@ import akka.actor.ActorSystem
 import akka.stream.ActorMaterializer
 import akka.stream.scaladsl._
 
-object SampleApp extends App {
+object SimpleFlowGraph extends App {
 
   implicit val system = ActorSystem("Sys")
   implicit val materializer = ActorMaterializer()
+
+//  Source(1 to 3)
+//    .map { i => println(s"A: $i"); i }
+//    .map { i => println(s"B: $i"); i }
+//    .map { i => println(s"C: $i"); i }
+//    .runWith(Sink.onComplete (_ => system.shutdown() ))
 
   val graph = FlowGraph.closed() { implicit builder: FlowGraph.Builder[Unit] =>
     import FlowGraph.Implicits._
